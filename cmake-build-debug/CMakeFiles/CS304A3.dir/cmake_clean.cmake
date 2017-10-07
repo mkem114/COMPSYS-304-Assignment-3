baeaf9da81d02888beea1fa8972ef27d759d93bf
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-07 16:52:21 +1300

    first commit

---
+++ cmake-build-debug/CMakeFiles/CS304A3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS304A3.dir/assignment.c.obj"
+  "CS304A3.pdb"
+  "CS304A3.exe"
+  "CS304A3.exe.manifest"
+  "libCS304A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS304A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
